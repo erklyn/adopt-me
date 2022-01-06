9ea8d5ddefb81716b062233e9982a2e3c4e14dfb
erklyn <uenesgursel@gmail.com>
2022-01-06 12:56:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+const Pet = (props) => {
+    return React.createElement("div", {} , [
+        React.createElement("h2" , {} , props.name),
+        React.createElement("h3" , {} , props.animal),
+        React.createElement("h3" , {} , props.breed),
+    ]);
+};
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {} ,
+        [
+            React.createElement("h1", {},`Adopt Me!`),
+            React.createElement(Pet , { name: "Lumos", animal:"Cat" , breed:"Siyamese"}),
+            React.createElement(Pet , { name: "Peyami", animal:"Cat" , breed:"Tekir"}),
+            React.createElement(Pet , { name: "Alfa", animal:"Cat" , breed:"Tekir"}),
+        ]
+    );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
